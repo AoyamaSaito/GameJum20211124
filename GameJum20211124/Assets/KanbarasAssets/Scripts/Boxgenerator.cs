440296791bb18cad30fd1b786754f34c712aeac9
Ryusei1115 <tekunikaru22@gmail.com>
2021-11-24 12:54:41 +0900

    あああああ

---
+++ GameJum20211124/Assets/KanbarasAssets/Scripts/Boxgenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class Boxgenerator : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_gameobject = default;
+    int[] m_box = new int[]{0, 1, 2, 3};
+    int[] m_shuffle = new int[4];
+
+    void Start()
+    {
+        Generator();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Generator()
+    {
+        m_shuffle = m_box.ToList().OrderBy(i => Guid.NewGuid()).ToArray();
+        for (int i = 0; i < m_gameobject.Length; i++)
+        {
+            GameObject go = Instantiate(m_gameobject[m_shuffle[i]]);
+        }
+    }
+}
