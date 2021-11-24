b952e7a6e228fa6750ce87413a395815dc9e2299
aoyama_saito <saitomarimo1229@gmail.com>
2021-11-24 11:34:51 +0900

    メイン画面の操作部分を追加

---
+++ GameJum20211124/Assets/Aoyama/FallMoney.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallMoney : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float minTorque = -30;
+    [SerializeField] float maxTorque = 30;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddTorque(Random.Range(minTorque, maxTorque));
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
