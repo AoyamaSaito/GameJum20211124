4f2deaf63def28d3373aa634ff19f0484e0444f2
aoyama_saito <saitomarimo1229@gmail.com>
2021-11-24 14:28:00 +0900

    フォントを追加

---
+++ GameJum20211124/Assets/Aoyama/Script/TheGoal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheGoal : MonoBehaviour
+{
+    [SerializeField] int[] goals = new int[5];
+    [SerializeField] int currentGoalIndex = 0;
+    [SerializeField] GameObject missText = default;
+    [SerializeField] float missTextTime = 1f;
+    [SerializeField] Text goalText;
+
+    int goal = 0;
+
+    void Start()
+    {
+        goal = goals[currentGoalIndex++];
+        goalText.text = "目標金額：" + goal;
+    }
+
+    public void Buy()
+    {
+        if(MoneyManager.CurrentMoney >= goal)
+        {
+            MoneyManager.ReduceMoney(goal);
+            goalText.text = "目標金額：" + goal;
+            goal = goals[currentGoalIndex++];
+        }
+        else
+        {
+            StartCoroutine(MissText());
+        }
+    }
+
+    IEnumerator MissText()
+    {
+        missText.SetActive(true);
+        yield return new WaitForSeconds(missTextTime);
+        missText.SetActive(false);
+    }
+}
