e919e6da2f2ea6b966ae7f9d4c42053a4a50c616
SinoharaRyuga <raitan3170@gmail.com>
2021-11-24 14:14:06 +0900

    関数をstaticに変更

--- GameJum20211124/Assets/Shinohara/Scripts/MoneyManager.cs
+++ GameJum20211124/Assets/Shinohara/Scripts/MoneyManager.cs
@@ -6,45 +6,59 @@ public class MoneyManager : MonoBehaviour
 {
     [Tooltip("テスト用所持金")]
     [SerializeField] int _testMoney = 0;
-    [Tooltip("テスト用上限")]
-    [SerializeField] int _testMaxMoney = 0;
-    [SerializeField] Text _text = default;
-    [SerializeField] Text _maxText = default;
-
+    [Tooltip("最初の上限")]
+    [SerializeField] int _firstMaxMoney = 0;
     /// <summary>時間経過で増える量</summary>
     [SerializeField] int _addMoneyAmount = 0;
     /// <summary>お金をが増える間隔</summary>
     [SerializeField] float _addMoneyInterval = 0f;
+
     /// <summary>現在の所持金</summary>
     static int _currentMoney = 0;
     /// <summary>所持金の上限</summary>
     static int _maxMoney = 0;
-    
+
+    static int _addAmount = 0;
+
+    static float _addInterval = 0f;
+
     /// <summary>インスタンスを取得するためのパブリック変数</summary>
     public static MoneyManager Instance = default;
 
-    float _time = 0f;
+    static float _time = 0f;
 
     /// <summary>現在の所持金</summary>
     public static int CurrentMoney { get => _currentMoney; set => _currentMoney = value; }
     /// <summary>所持金の上限</summary>
     public static int MaxMoney { get => _maxMoney; set => _maxMoney = value; }
 
+    /// <summary>シングルトン</summary>
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         _currentMoney = _testMoney;
-        _maxMoney = _testMaxMoney;
-        Debug.Log($"所持金 {_currentMoney}");
-        Debug.Log($"上限　{_maxMoney}");
+        _maxMoney = _firstMaxMoney; //最初の上限を設定する
+        _addInterval = _addMoneyInterval;
+        _addAmount = _addMoneyAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
         _time += Time.deltaTime;
-        _text.text = "所持金" + _currentMoney.ToString();
-        _maxText.text = "上限" + _maxMoney.ToString();
         TimeMoney();
         
     }
@@ -53,7 +67,6 @@ public class MoneyManager : MonoBehaviour
     /// <param name="addmoney">増やす量</param>
     public static void AddMoney(int addMoney)
     {
-        Debug.Log("上がる");
         _currentMoney = Mathf.Min(_currentMoney  + addMoney, _maxMoney);
     }
 
@@ -61,7 +74,6 @@ public class MoneyManager : MonoBehaviour
     /// <param name="reduceMoney">減らす量</param>
     public static void ReduceMoney(int reduceMoney)
     {
-        Debug.Log("下がる");
         _currentMoney = Mathf.Max(_currentMoney - reduceMoney, 0);
     }
 
@@ -69,22 +81,19 @@ public class MoneyManager : MonoBehaviour
     /// <param name="nextMaxMoney">次の上限</param>
     public static void UpMaxMoney(int nextMaxMoney)
     {
-        Debug.Log("上限を上げる");
         _maxMoney = nextMaxMoney;
     }
 
     /// <summary>時間経過で増えるお金</summary>
-    private void TimeMoney()
+    private static void TimeMoney()
     {
-        if (_time >= _addMoneyInterval && _currentMoney < _maxMoney)
+        if (_time >= _addInterval && _currentMoney < _maxMoney)
         {
-            Debug.Log($"所持金 {_currentMoney}");
-            _currentMoney += _addMoneyAmount;
+            _currentMoney += _addAmount;
             _time = 0f;
         }
-        else if (_time >= _addMoneyInterval)
+        else if (_time >= _addInterval)
         {
-            Debug.Log($"所持金上限");
             _time = 0f;
         }
     }
