209a351b19278145ed79bdfa1c84b57bcd9184b9
Ryusei1115 <tekunikaru22@gmail.com>
2021-11-24 16:23:58 +0900

    バイトが完成

--- GameJum20211124/Assets/KanbarasAssets/Scripts/BoxNumber.cs
+++ GameJum20211124/Assets/KanbarasAssets/Scripts/BoxNumber.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BoxNumber : MonoBehaviour
 {
     public GameObject m_gameObject = default;
     Boxgenerator bg;
 
-    private void Start()
+    void Start()
     {
      
     }
@@ -17,20 +18,28 @@ public class BoxNumber : MonoBehaviour
         bg = GameObject.Find("Generator").GetComponent<Boxgenerator>();
         bg.count++;
         Debug.Log($"Box{bg.count}");
+        Debug.Log(m_gameObject.name);
 
-        if(m_gameObject.name == $"Box{bg.count}")
+        if(m_gameObject.name == $"Box{bg.count}(Clone)")
         {
             bg.answerCount++;
             Debug.Log(bg.answerCount);
         }
         else
         {
-            //miss
+            MoneyManager.ReduceMoney(MoneyManager.CurrentMoney/10);
         }
 
         if(bg.answerCount == 4)
         {
-            //clear
+            MoneyManager.AddMoney(MoneyManager.CurrentMoney / 10);
+            StartCoroutine(StageChange());
         }
+    }
+
+    IEnumerator StageChange()
+    {
+        yield return new WaitForSeconds(bg.stageChange);
+        SceneManager.LoadSceneAsync("MainScene");//シーンの名前を入れる
     }
 }
